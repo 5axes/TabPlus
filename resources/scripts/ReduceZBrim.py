166a1b6555dfb92e0c9cbb2d8d8f9c1154b9bf31
5axes <5axes@users.noreply.github.com>
2022-06-04 16:42:17 +0200

    Update ReduceZBrim.py

--- resources/scripts/ReduceZBrim.py
+++ resources/scripts/ReduceZBrim.py
@@ -180,18 +180,28 @@ class ReduceZBrim(Script):
                     "minimum_value": 0.06,
                     "maximum_value_warning": 0.2,
                     "maximum_value": 0.3
-                }               
+                },
+                "extruder_nb":
+                {
+                    "label": "Extruder Id",
+                    "description": "Define extruder Id in case of multi extruders",
+                    "unit": "",
+                    "type": "int",
+                    "default_value": 1
+                }                
             }
         }"""
 
     def execute(self, data):
 
-        BrimReduce = int(self.getSettingValueByKey("reduce"))        
-
+        BrimReduce = float(self.getSettingValueByKey("reduce"))   
+        Logger.log('d', 'BrimReduce : {}'.format(BrimReduce))            
+        extruder_id  = self.getSettingValueByKey("extruder_nb")
+        extruder_id = extruder_id -1
+        
+        
         idl=0
-        lines_brim =[]
         StartLine=''
-        BrimF='F0'
         FirstZToReplace=''
         InitialE=''
         StartZ=0
@@ -199,29 +209,39 @@ class ReduceZBrim(Script):
         xyline=''
         nb_line=0
         currentlayer=0
-        CurrentE=0
-        RetractE=0
-        ResetE=0
-        lastE='G92 E0'
-        RetractF=3000
-        RelativeExtruder = False
+
         
+
+        # Deprecation function
+        # extrud = list(Application.getInstance().getGlobalContainerStack().extruders.values())
+        extrud = Application.getInstance().getGlobalContainerStack().extruderList
+ 
+        layer_height_0 = extrud[extruder_id].getProperty("layer_height_0", "value")
+        Logger.log('d', 'layer_height_0 : {}'.format(layer_height_0))
+        
+        layer_reduction = int((BrimReduce/layer_height_0)*100)
+        Logger.log('d', 'layer_reduction : {}'.format(layer_reduction))
+
+
+        #   machine_extruder_count
+        extruder_count=Application.getInstance().getGlobalContainerStack().getProperty("machine_extruder_count", "value")
+        extruder_count = extruder_count-1
+        if extruder_id>extruder_count :
+            extruder_id=extruder_count
+
+            
         for layer in data:
             layer_index = data.index(layer)
             
             lines = layer.split("\n")
             for line in lines:                  
                
-                if is_relative_extrusion_line(line):
-                    RelativeExtruder = True
-                
-                if is_absolute_extrusion_line(line):
-                    RelativeExtruder = False
                     
                 if line.startswith(";LAYER_COUNT:"):
                     # Logger.log("w", "found LAYER_COUNT %s", line[13:])
                     layercount=int(line[13:])                    
                
+                # startswith ";LAYER"
                 if is_begin_layer_line(line):
                     line_index = lines.index(line)    
                     # Logger.log('d', 'layer_lines : {}'.format(line))
@@ -229,85 +249,18 @@ class ReduceZBrim(Script):
                     # Logger.log('d', 'currentlayer : {:d}'.format(currentlayer))
                     if line.startswith(";LAYER:0"):
                         idl=1
-                    
-                    # Copy the Original Brim
-                    elif currentlayer <= BrimReduce :
-                        # Logger.log('d', 'Insert Here : {:d}'.format(currentlayer))
-                        # Logger.log('d', 'First   Z   : {}'.format(FirstZToReplace))
-                        line_index = lines.index(line)
-                        xyline=lines[line_index-3]
-                        
-                        #----------------------------
-                        #    Begin of modification
-                        #----------------------------
-                        nb_line = 1
-                        lines.insert(line_index + nb_line, ";BEGIN_OF_MODIFICATION")
-                        # Logger.log('d', 'xyline   : {}'.format(xyline))
-                        # Reset the Extruder position
-                        if RetractE >0 and RelativeExtruder == False :
-                            nb_line+=1
-                            lines.insert(line_index + nb_line, "G1 F" + str(RetractF) +  " E" + str(ResetE) )
-                        
-                        if RelativeExtruder == False :
-                            nb_line+=1
-                            lines.insert(line_index + nb_line, InitialE)
-                        
-                        #    Set Z position of the Brim
-                        searchZ = re.search(r"Z(\d*\.?\d*)", StartLine)
-                        if searchZ:
-                            FirstZToReplace="Z"+searchZ.group(1)                       
-                            ModiZ="Z"+str(round((float(searchZ.group(1)   )+BrimZ),5))                          
-                                               
-                        BeginLine=StartLine.replace(FirstZToReplace, ModiZ)
-                        
-                        nb_line+=1
-                        lines.insert(line_index + nb_line, BeginLine)
 
-                        for aline in lines_brim:
-                            nb_line+=1
-                            searchZ = re.search(r"Z(\d*\.?\d*)", aline)
-                            if searchZ:
-                                Cz="Z"+searchZ.group(1)                       
-                                ModiZ="Z"+str(round((float(searchZ.group(1)   )+BrimZ),5))  
-                                # Logger.log('d', 'Current Z   : {}'.format(Cz))
-                                # Logger.log('d', 'Modi    Z   : {}'.format(ModiZ))
-                                InsertLine=aline.replace(Cz, ModiZ)
-                            else:
-                                InsertLine=aline
-                            lines.insert(line_index + nb_line, InsertLine)
-                        nb_line+=1
-                        lines.insert(line_index + nb_line, xyline)
-                        nb_line+=1
-                        lines.insert(line_index + nb_line, "G1 Z"+str(currentz))
-                        # Reset Etruder position
-                        if RelativeExtruder == False:
-                            nb_line+=1
-                            lines.insert(line_index + nb_line, lastE)
-                        
-                        #----------------------------
-                        #    End of modification
-                        #----------------------------
-                        nb_line+=1
-                        lines.insert(line_index + nb_line, ";END_OF_MODIFICATION")
-                        BrimZ += StartZ
-     
                 if idl == 2 and is_begin_type_line(line):
                     idl = 0
+                    line_index = lines.index(line)   
                     
-                if idl == 2 and is_begin_mesh_line(line) :
-                    idl = 0
-                
-                #---------------------------------------
-                # Add the Brim line to the brim path 
-                #---------------------------------------                
-                if idl == 2 :
-                    # if not is_only_extrusion_line(line):
-                    cline = line
-                    lines_brim.append(cline)
-                
-                #---------------------------------------
-                # Init copy of the BRIM extruding path
-                #---------------------------------------
+                    lines.insert(line_index , ";END_OF_MODIFICATION")
+                    lines.insert(line_index , "M221 S100")
+                    lines.insert(line_index , "G0 Z"+str(layer_height_0))
+                    
+                #---------------------------------------------------
+                # Init modification of the BRIM extruding path
+                #---------------------------------------------------
                 # G0 F6000 X106.445 Y116.579 Z0.2   -> StartLine
                 # ;TYPE:SKIRT
                 # G1 F3000 E0                       -> ZHopLine/ELine
@@ -330,7 +283,6 @@ class ReduceZBrim(Script):
 
                 if idl == 1 and is_begin_skirt_line(line):
                     idl=2
-                    InitialE=''
                     
                     # StartLine get the Z height
                     line_index = lines.index(line)-1
@@ -349,47 +301,20 @@ class ReduceZBrim(Script):
                     BrimZ = StartZ
                     # Logger.log('d', 'BrimZ   : {:f}'.format(BrimZ))                    
  
-                    # Logger.log('d', 'ZHopLine   : {}'.format(ZHopLine))
-                    searchE = re.search(r"E([-+]?\d*\.?\d*)", ZHopLine)
-                    if searchE:
-                        InitialE="G92 E"+str(searchE.group(1))
-                        nb_line=3
-                    else:
-                        ZHopLine=lines[line_index+3]
-                        searchE = re.search(r"E([-+]?\d*\.?\d*)", ZHopLine)
-                        if searchE and InitialE=='' :
-                            InitialE="G92 E"+str(searchE.group(1))    
-                        nb_line=4
+
+                    #----------------------------
+                    #    Begin of modification
+                    #----------------------------
+                    lines.insert(line_index + 2, ";BEGIN_OF_MODIFICATION")                    
+                    lines.insert(line_index + 3, "G0Z" + str(BrimReduce) )
+                    lines.insert(line_index + 4, "M221 S" + str(layer_reduction) )
                     
-                    SpeedLine=lines[line_index+nb_line]
-                    # Logger.log('d', 'SpeedLine   : {}'.format(SpeedLine))
-                    searchF = re.search(r"F(\d*\.?\d*)", SpeedLine)
-                    if searchF:
-                        BrimF="F"+searchF.group(1)                    
-                        # Logger.log('d', 'BrimF     : {}'.format(BrimF))
-                    
-                    lines_brim =[]
-                    startlayer=currentlayer
-                    lines_brim.append(line)
-                     
-                
-                if currentlayer <= BrimReduce and is_z_line(line):
+                if  is_z_line(line) and idl:
                     searchZ = re.search(r"Z(\d*\.?\d*)", line)
                     if searchZ:
                         currentz=float(searchZ.group(1))
-
-                if currentlayer <= BrimReduce and is_e_line(line):
-                    searchE = re.search(r"E([-+]?\d*\.?\d*)", line)
-                    if searchE:
-                        lastE="G92 E"+searchE.group(1)
-                        RetractE=CurrentE-float(searchE.group(1))
-                        ResetE=CurrentE
-                        CurrentE=float(searchE.group(1))
-                    
-                        searchF = re.search(r"F(\d*\.?\d*)", line)
-                        if searchF:
-                            RetractF=float(searchF.group(1))
-                        
+                
+                
             result = "\n".join(lines)
             data[layer_index] = result
 
